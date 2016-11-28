fdbdd004a156553f186a46baae799e4e708246a7
Jon Chappelow <chappjc@gmail.com>
2016-11-27 18:44:02 -0800

    Hash in version option.

--- version.go
+++ version.go
@@ -11,21 +11,25 @@ type version struct {
 var ver = version{
 	Major: 0,
 	Minor: 7,
-	Patch: 0,
-	Label: "alpha",
+	Patch: 2,
+	Label: "beta",
 	Nick:  "Fletch"}
 
 // CommitHash may be set on the build command line:
-// go build -ldflags "-X main.CommitHash=`git rev-parse HEAD`"
-// var CommitHash string
+// go build -ldflags "-X main.CommitHash=`git rev-parse --short HEAD`"
+var CommitHash string
 
 const appName string = "dcrspy"
 
 func (v *version) String() string {
-	if v.Label != "" {
-		return fmt.Sprintf("%d.%d.%d-%s \"%s\"",
-			v.Major, v.Minor, v.Patch, v.Label, v.Nick)
+	var hashStr string
+	if CommitHash != "" {
+		hashStr = "+" + CommitHash
 	}
-	return fmt.Sprintf("%d.%d.%d \"%s\"",
-		v.Major, v.Minor, v.Patch, v.Nick)
+	if v.Label != "" {
+		return fmt.Sprintf("%d.%d.%d-%s%s \"%s\"",
+			v.Major, v.Minor, v.Patch, v.Label, hashStr, v.Nick)
+	}
+	return fmt.Sprintf("%d.%d.%d%s \"%s\"",
+		v.Major, v.Minor, v.Patch, hashStr, v.Nick)
 }
